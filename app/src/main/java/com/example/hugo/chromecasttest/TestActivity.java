e61f1800f476f863a7f6cb5474615b70c1d5decd
hbrisson <nobodyelpapa@gmail.com>
2016-01-31 23:11:35 -0500

    Working Discovery button

--- app/src/main/java/com/example/hugo/chromecasttest/TestActivity.java
+++ app/src/main/java/com/example/hugo/chromecasttest/TestActivity.java
@@ -3,13 +3,25 @@ package com.example.hugo.chromecasttest;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.MediaRouteActionProvider;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.cast.CastDevice;
+import com.google.android.gms.cast.CastMediaControlIntent;
+
 public class TestActivity extends AppCompatActivity {
+
+    private MediaRouter mMediaRouter;
+    private MediaRouteSelector mMediaRouteSelector;
+    private MediaRouter.Callback mMediaRouterCallback;
+    private CastDevice mCastDevice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +29,31 @@ public class TestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // STEP 01 - Obtain an instance of MediaRouter and keep it for the lifetime of the app
+
+        mMediaRouter = MediaRouter.getInstance(getApplicationContext());
+
+        // STEP 02 - Filter the discovery of Chromecast to eligible devices
+        // TODO Replace Application ID or keep default
+        mMediaRouteSelector = new MediaRouteSelector.Builder()
+                .addControlCategory(CastMediaControlIntent.categoryForCast(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID))
+                .build();
+
+        // STEP 04 - Register Callbacks for MediaRouteSelector
+        mMediaRouterCallback = new MediaRouter.Callback() {
+            @Override
+            public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo route) {
+                super.onRouteSelected(router, route);
+                mCastDevice = CastDevice.getFromBundle(route.getExtras());
+            }
+
+            @Override
+            public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo route) {
+                super.onRouteUnselected(router, route);
+                mCastDevice = null;
+            }
+        };
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,7 +69,28 @@ public class TestActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_test, menu);
+
+        // STEP 03 - Use the selector to filter the devices that are displayed to the user when the Cast button in the ActionBar
+        MenuItem mediaRouteMenuItem = menu.findItem(R.id.media_route_menu_item);
+        MediaRouteActionProvider mediaRouteActionProvider =
+                (MediaRouteActionProvider) MenuItemCompat.getActionProvider(mediaRouteMenuItem);
+        mediaRouteActionProvider.setRouteSelector(mMediaRouteSelector);
+
         return true;
+    }
+
+    // STEP 05 - Add callback to media Router
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mMediaRouter.addCallback(mMediaRouteSelector, mMediaRouterCallback,
+                MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY);
+    }
+
+    @Override
+    protected void onStop() {
+        mMediaRouter.removeCallback(mMediaRouterCallback);
+        super.onStop();
     }
 
     @Override
@@ -40,12 +98,6 @@ public class TestActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
